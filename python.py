e13cd235ffad814f1f251d8262d44a98ac2a1f5a
zufarbekjumayev <jumayevzufarbek829@gmail.com>
2024-12-05 16:08:09 +0300

    python1

---
+++ python.py
@@ -0,0 +1,20 @@
+a = "Restoran Buyurtma ma'lumotlari"
+print(a)
+b = "Joe"
+print("Mijozning ismi:", b)
+c = "Pizza"
+print("Taom nomi:", c)
+d = "Cola"
+print("Ichimlik nomi:", d)
+s = 2
+print("Buyurtma miqdor:", s)
+g = 12.99
+print("Taom narxi:", g, "USD")
+j = 2.5
+print("Ichimlik narxi:", j, "USD")
+p = 12.99 * 2 + 2.5 * 2
+print("Umumiy narx (Taom va ichimliklar):", p, "USD")
+e = p / 10
+print("Xizmat haqi (10%):", e, "USD" )
+t = p + e
+print("Jami narx:", t, "USD")
